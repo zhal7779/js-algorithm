92f30e1822f66a66638c6b84c8ae6e1293113b0a
Minyoung Lee <zhal7779@gmail.com>
2024-02-13 23:44:04 +0900

    1.13 대소문자 변환

---
+++ Section1. 기본문제/13. 대소문자 변환.js
@@ -0,0 +1,14 @@
+//대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+function solution(str) {
+  let answer = '';
+  for (let s of str) {
+    if (s === s.toLowerCase()) {
+      answer += s.toUpperCase();
+    } else {
+      answer += s.toLowerCase();
+    }
+  }
+  return answer;
+}
+
+console.log(solution('StuDY'));
