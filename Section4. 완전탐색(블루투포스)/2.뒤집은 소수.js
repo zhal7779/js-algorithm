0501fdbccb79178bf5079d398cc3df337c12a24c
minyoung <zhal7779@naver.com>
2024-03-10 22:39:53 +0900

    4.3 멘토링

--- Section4. 완전탐색(블루투포스)/2.뒤집은 소수.js
+++ Section4. 완전탐색(블루투포스)/2.뒤집은 소수.js
@@ -27,4 +27,4 @@ function solution (numbers) {
     return answer;
 }
 
-console.log(solution(['32', '55', '62', '20', '250', '370', '200', '30', '100']))
+console.log(solution(['32', '55', '62', '20', '250', '370', '200', '30', '100']));
