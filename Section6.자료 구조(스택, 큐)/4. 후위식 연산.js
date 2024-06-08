75fc6b45f01a9fc2069c077fa1e8eb62a3784fb1
minyoung <zhal7779@naver.com>
2024-06-08 21:09:10 +0900

    6.4 후위식 연산

---
+++ Section6.자료 구조(스택, 큐)/4. 후위식 연산.js
@@ -0,0 +1,28 @@
+// 후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
+// 만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 12입니다.
+function solution(str) {
+    let answer = 0;
+    const stack = [];
+    for(let i = 0; i<str.length; i++){
+        if(!isNaN(str[i])) {
+            stack.push(str[i]);
+        } else {
+            let firstNum = stack.pop();
+            let secondNum = stack.pop();
+            if(str[i] === '+') {
+                stack.push(firstNum + secondNum)
+            } else if(str[i] === '-') {
+                stack.push(firstNum - secondNum)
+            } else if(str[i] === '*') {
+                stack.push(firstNum * secondNum)
+            } else if(str[i] === '/') {
+                stack.push(firstNum / secondNum)
+            } 
+        }
+
+    }
+    answer = stack[0];
+    return answer;
+}
+
+console.log(solution("352+*9-"))
