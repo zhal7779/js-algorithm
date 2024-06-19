cff97736de2e8de35ad9e0c345a7febd4e7cccd8
minyoung <zhal7779@naver.com>
2024-06-19 14:28:07 +0900

    7.4 삽입 정렬

---
+++ Section7. 정렬과 그리디, 결정 알고리즘/4. 삽입 정렬.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+console.log(solution([11, 7, 5, 6, 10, 9]));
