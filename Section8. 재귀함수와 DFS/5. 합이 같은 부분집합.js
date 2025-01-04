466e00b2e3c37d3e59cbfda61058ff6d712c42b3
minyoung <zhal7779@naver.com>
2025-01-04 23:45:13 +0900

    Review: 8.5 합이 같은 부분집합(이진트리 DFS)

--- Section8. 재귀함수와 DFS/5. 합이 같은 부분집합.js
+++ Section8. 재귀함수와 DFS/5. 합이 같은 부분집합.js
@@ -1,12 +1,14 @@
 function solution(arr) {
   let answer = "NO";
+  let flag = 0;
   const total = arr.reduce((acc, cur) => acc + cur, 0);
 
   function recursion(i, sum) {
+    if (flag) return;
     if (i === arr.length) {
       if (sum === total - sum) {
-        console.log(sum);
         answer = "YES";
+        flag = 1;
       }
       return;
     }
